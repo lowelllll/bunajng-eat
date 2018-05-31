6be497c1bc15a164bdee64080b709288f66cf14d
lowelll <yejin2651@naver.com>
2018-05-31 17:48:41 +0900

    select_menu 기능 구현
    랜덤으로 (먹은지 7일 이상된)메뉴를 골라주는 기능 구현.

--- API/menubot/menu/views.py
+++ API/menubot/menu/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import datetime
 from .models import Menu
+
 # Create your views here.
 
 def select_menu(request):
     data = {}
     if request.method == 'GET':
-        menu = Menu.objects.random()
-        data['menu'] = list(menu)
+        today = datetime.datetime.now(datetime.timezone.utc)
+        while True:
+            # 1부터 현재 모델 갯수 -1 까지의 난수
+            menu = Menu.objects.random()
+            if not menu.last_date:
+                break
+            dt = today - menu.last_date
+            if dt.days > 6:
+               break
+            print(menu.name+"먹음")
+        data = {'id':menu.pk,'name':menu.name,'last_date':menu.last_date}
+        # menu.last_date = today
+        # menu.save()
     return JsonResponse(data)
+
+def ate_menu(request,pk):
+    return True
 
 def create_menu(request):
     return JsonResponse()
