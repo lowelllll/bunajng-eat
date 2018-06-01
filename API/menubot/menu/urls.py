1ca74fa0aeadb946c2a3736e3ad1c83ec48cf006
lowelll <yejin2651@naver.com>
2018-06-01 14:59:14 +0900

    create_menu 기능 구현
    메뉴를 생성하는 기능

--- API/menubot/menu/urls.py
+++ API/menubot/menu/urls.py
@@ -6,4 +6,5 @@ app_name = 'menu'
 urlpatterns = [
     url('^$',select_menu,name='select_menu'),
     url('^menus/$',list_menu,name='list_menu'),
+    url('^create/$',create_menu,name='create_menu'),
 ]
--- API/menubot/menu/views.py
+++ API/menubot/menu/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import datetime
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import ensure_csrf_cookie,requires_csrf_token
 from .models import Menu
 
 # Create your views here.
 
 def select_menu(request): # 랜덤으로 메뉴를 고름
     data = {}
+
     if request.method == 'GET':
         today = datetime.datetime.now(datetime.timezone.utc)
         while True:
@@ -16,18 +19,39 @@ def select_menu(request): # 랜덤으로 메뉴를 고름
             dt = today - menu.last_date
             if dt.days > 6:
                break
-            print(menu.name+"먹음")
-        data = {'id':menu.pk,'name':menu.name,'last_date':menu.last_date}
+        data = {
+            'id':menu.pk,
+            'name':menu.name,
+            'last_date':menu.last_date
+        }
         # menu.last_date = today
         # menu.save()
+
     return JsonResponse(data)
 
 def ate_menu(request,pk): # 먹은 메뉴를 확인
     return True
 
+# @requires_csrf_token
+# @ensure_csrf_cookie
+@csrf_exempt # csrf 임시 해제
 def create_menu(request): # 새로운 메뉴 생성
-    return JsonResponse()
+    data = {}
 
+    if request.method == 'POST':
+        name = request.POST['name']
+        menu = Menu(name=name)
+        menu.save()
+
+        data = {
+            'id':menu.pk,
+            'name':menu.name,
+            'last_date':menu.last_date
+        }
+
+    return JsonResponse(data)
+
+@csrf_exempt
 def delete_menu(request): # 메뉴 삭제
     return JsonResponse()
 
