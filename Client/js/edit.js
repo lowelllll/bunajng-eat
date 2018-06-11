be6fb92d7c8e21893a1e6997e44783de924668f2
munjimon <qkrrjs0411@naver.com>
2018-06-11 11:50:23 +0900

    코드 개선
    목록 수정에 Enter Press가 동작하게 구현,
    목록 추가 시 Input inner value 초기화,
    목록 약간의 UI 수정,
    삭제 버튼 클릭 시 재확인 메세지 띄우기 완료
    자잘한 문법 수정

--- Client/js/edit.js
+++ Client/js/edit.js
@@ -16,7 +16,8 @@ $.fn.serializeObject = function()
 };
 
 // 버튼 색 변화
-function mykeydown() {
+const input = document.getElementById('footer__changer');
+input.onkeydown = function() {
         document.getElementById('footer__changer').addEventListener('keyup', function(e){
             const footerSubmitEl = document.getElementById("footer__submit")
             if(e.target.selectionStart === 0){
@@ -31,10 +32,9 @@ function mykeydown() {
 
 }
 
-// 메뉴 생
+// 메뉴 추가
 $("#footer__submit").on("click",() => { // submit
     // e.preventDefault();
-
     let FormData  = $("#footer__changer").val();
     console.log(FormData);
     $.ajax({
@@ -69,57 +69,58 @@ $("#footer__submit").on("click",() => { // submit
             console.log("error");
         }
     })
-
+    return false;
 });
 
-// 삭제
+// 메뉴 삭제
 $("#menus").on("click", ".menu__delete", function() {
-    const $menus = $(this).parent();
-    const $input = $menus.find(".id");
-    const pk = $input.val();
-    console.log('id', pk);
+    const check = confirm("삭제하시겠습니까?");
+    if(check === true){
+        const $menus = $(this).parent();
+        const $input = $menus.find(".id");
+        const pk = $input.val();
+        console.log('id', pk);
 
-    $.ajax({
-        url:"//menubot.pythonanywhere.com/menu/delete/",
-        dataType:'json',
-        type:'POST',
-        data:{
-            "pk":pk
-        },
-        success:function () {
-            //alert("success")
-        },
-        error:function () {
-            alert("error")
-        }
-    })
-
-
-    $($menus).remove();
+        $.ajax({
+            url:"//menubot.pythonanywhere.com/menu/delete/",
+            dataType:'json',
+            type:'POST',
+            data:{
+                "pk":pk
+            },
+            success:function () {
+                //alert("success")
+            },
+            error:function () {
+                alert("error");
+            }
+        })
+        $($menus).remove();
+    }
 });
+
+// 마지막으로 먹은 날 계산
 
 function date_calc(temp_date){
-    let date = new Date();
-    let yy = date.getFullYear();
+    const date = new Date();
+    const yy = date.getFullYear();
     let mm = date.getMonth()+1;
     let dd = date.getDate();
     if(dd<10) { dd='0'+dd };
     if(mm<10) { mm='0'+mm };
-    let temp_today = `${yy}-${mm}-${dd}`;
-    let s = "2018-05-21";
+    const temp_today = `${yy}-${mm}-${dd}`;
     let result = null;
 
     if(!(temp_date === null)){
         temp_date = temp_date.toString().substr(0,10);
-        let dateArray = temp_date.split("-");
-        console.log(dateArray);
-        let last_date = new Date(dateArray[0],dateArray[1],dateArray[2]);
+        const dateArray = temp_date.split("-");
+        const todayArray = temp_today.split("-");
 
-        let todayArray = temp_today.split("-")
-        let today = new Date(todayArray[0],todayArray[1],todayArray[2]);
+        const last_date = new Date(dateArray[0],dateArray[1],dateArray[2]);
+        const today = new Date(todayArray[0],todayArray[1],todayArray[2]);
 
 
-        let dt = (today.getTime() - last_date.getTime())/ 1000 / 60 / 60 / 24;;
+        const dt = (today.getTime() - last_date.getTime())/ 1000 / 60 / 60 / 24;;
 
         if(dt === 0){
             result = "오늘 먹음";
@@ -131,8 +132,13 @@ function date_calc(temp_date){
             result = last_date;
         }
     }
+    else{
+        result = "먹지 않음";
+    }
     return result;
 }
+
+// 페이지 로딩 시 전체 리스트를 출력
 
 $.ajax({
     url:'//menubot.pythonanywhere.com/menu/menus/',
@@ -148,7 +154,7 @@ $.ajax({
                 <div class="menu">
                     <div class="menu__text">
                         <span class="menu__name">${name}</span>
-                        <span class="menu__last-date">${last_date===null?"먹지 않음":last_date}</span>
+                        <span class="menu__last-date">${last_date}</span>
                     </div>
                     <input type="hidden" class="id" value="${id}" />
                     <div class="menu__delete">
